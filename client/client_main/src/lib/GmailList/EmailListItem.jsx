8132ace60bdfd35746ba268bfa88737ea1f48655
Saphal <69297872+saphalpdyl@users.noreply.github.com>
2023-03-12 12:42:57 +0545

    Github Actions : Jest tests  (#2)
    * ADDED : Github Action for testing
    
    * CHANGES : Updated node js in workflow
    
    * Github Actions will run on any branch push
    
    * Updated BUG
    
    * Check once again
    
    ---------
    
    Co-authored-by: Saphal <saphal@2400-1A00-B010.ip6.wlink.com.np>

--- client/client_main/src/lib/GmailList/EmailListItem.jsx
+++ client/client_main/src/lib/GmailList/EmailListItem.jsx
@@ -1,3 +1,10 @@
+import formatDate from '../utils/dateFormatter';
+
+const options1 = {
+  is12hour: true,
+  dateCode: 'en-US',
+};
+
 const EmailListItem = ({email}) => {
   return (
     <div className="email-list-item">
@@ -5,6 +12,7 @@ const EmailListItem = ({email}) => {
       <span className="email_sent_at">{email.sent_at}</span>
       <span className="email_content">{email.content}</span>
       <br /> <br />
+      <p>{formatDate('2023-03-08 13:36:23', options1)}</p>
     </div>
   );
 };
